3824035baeba4f574add36a3d0a0e2f7e756f3f5
Free-Palestine-4EVER <zzeidnaser@gmail.com>
2026-03-29 18:24:28 +0200

    Update Stripe allowed countries to match OJAR shipping spreadsheet (43 countries)

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -130,38 +130,18 @@ module.exports = async function handler(req, res) {
             },
             billing_address_collection: 'required',
             shipping_address_collection: {
-                // All Stripe-supported countries worldwide
+                // Countries OJAR ships to (per shipping spreadsheet, updated 2026-03-29)
                 allowed_countries: [
-                    'AC', 'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AQ', 'AR', 'AT', 'AU', 'AW', 'AX', 'AZ',
-                    'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS',
-                    'BT', 'BV', 'BW', 'BY', 'BZ',
-                    'CA', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CV', 'CW', 'CY', 'CZ',
-                    'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ',
-                    'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET',
-                    'FI', 'FJ', 'FK', 'FO', 'FR',
-                    'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT',
-                    'GU', 'GW', 'GY',
-                    'HK', 'HN', 'HR', 'HT', 'HU',
-                    'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IS', 'IT',
-                    'JE', 'JM', 'JO', 'JP',
-                    'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KR', 'KW', 'KY', 'KZ',
-                    'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
-                    'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MK', 'ML', 'MM', 'MN', 'MO', 'MQ', 'MR', 'MS', 'MT', 'MU',
-                    'MV', 'MW', 'MX', 'MY', 'MZ',
-                    'NA', 'NC', 'NE', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ',
-                    'OM',
-                    'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PY',
-                    'QA',
-                    'RE', 'RO', 'RS', 'RW',
-                    'SA', 'SB', 'SC', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST',
-                    'SV', 'SX', 'SZ',
-                    'TA', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ',
-                    'UA', 'UG', 'US', 'UY', 'UZ',
-                    'VA', 'VC', 'VE', 'VG', 'VN', 'VU',
-                    'WF', 'WS',
-                    'XK',
-                    'YE', 'YT',
-                    'ZA', 'ZM', 'ZW',
+                    // Europe
+                    'AD', 'AL', 'AT', 'BA', 'BE', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES',
+                    'FI', 'FR', 'GB', 'GR', 'HR', 'HU', 'IE', 'IT', 'LI', 'LT', 'MC', 'ME', 'MT',
+                    'NL', 'NO', 'PL', 'PT', 'RO', 'RS', 'SE', 'SI', 'SK',
+                    // GCC
+                    'AE', 'BH', 'KW', 'OM', 'QA', 'SA',
+                    // Middle East
+                    'LB',
+                    // USA
+                    'US',
                 ],
             },
             metadata: {
